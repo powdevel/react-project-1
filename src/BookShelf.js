f5c056bff471976338c2af2ba9c561e3a759e181
powdevel <luis.aveiga@gmail.com>
2017-11-09 11:31:35 -0500

    Fix: Export Bookshelf
    Was exporting book instead of bookshelf. I chanaged the indentation too.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import Book from './Book'
 
 class BookShelf extends React.Component {
     render() {
         return (
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
-                </ol>
-            </div>
+                <h2 className="bookshelf-title">Bookshelf component</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book/>
+                        </li>
+                    </ol>
+                </div>
             </div>
         )
     }
 }
 
-export default Book;
+export default BookShelf;

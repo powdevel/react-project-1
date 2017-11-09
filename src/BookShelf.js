96bad820db74aecfd4d0e0e42f640bb0ad9b73eb
powdevel <luis.aveiga@gmail.com>
2017-11-09 18:47:02 -0500

    Revert "fix: use books props instead of text"
    This reverts commit e2048ec7ff6cb6d273c1d33a1695a9f915ae65a6.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,17 +3,20 @@ import Book from './Book'
 
 class BookShelf extends React.Component {
     render() {
+
+        console.log(this.props);
+
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">
-                    { this.props.id.replace(/([a-z](?=[A-Z]))/g, '$1 ') }
+                    Bookshelf component
                 </h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {
                             this.props.books.map( e =>
-                                <li key={e.title} >
-                                    <Book data={e} >
+                                <li>
+                                    <Book data={e}>
                                     </Book>
                                 </li>
                             )

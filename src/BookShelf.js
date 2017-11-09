e43bb9dc35d80d5994dfc793e6563e629deb6033
powdevel <luis.aveiga@gmail.com>
2017-11-09 11:16:55 -0500

    Feat: Create Bookshelf
    Creating bookshelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class BookShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                    <li>
+                </ol>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Book;

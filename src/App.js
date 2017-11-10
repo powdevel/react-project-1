e07aa1e522eb1877faa9f16aa28f73364f03ffcd
powdevel <luis.aveiga@gmail.com>
2017-11-10 13:49:41 -0500

    refactor: create search component
    Creates search component and refactor app js to use this new component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelf from './BookShelf'
 import {Link, Route} from 'react-router-dom'
+import Search from './Search.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -60,27 +61,7 @@ class BooksApp extends React.Component {
 
     return (<div className="app">
 
-      <Route path="/search" exact={true} render={() => <div className="search-books">
-          <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close
-            </a>
-            <div className="search-books-input-wrapper">
-              {/* NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                  */
-              }
-              <input type="text" placeholder="Search by title or author"/>
-
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>}/>
+      <Route path="/search" exact={true} component={Search} />
 
       <Route path="/" exact={true} render={() => <div className="list-books">
           <div className="list-books-title">

d123c200c6eca1b9ed96ce6c3d64a7dce3045e63
powdevel <luis.aveiga@gmail.com>
2017-11-10 13:41:52 -0500

    feat: define links and routes for / and /search
    Defining routes and links with router and link components from
    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelf from './BookShelf'
-import {Link} from 'react-router-dom'
+import {Link, Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -59,46 +59,45 @@ class BooksApp extends React.Component {
     // console.log( this.state.shelfs );
 
     return (<div className="app">
-      {
-        this.state.showSearchPage
-          ? (<div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close
-              </a>
-              <div className="search-books-input-wrapper">
-                {/*
-                                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                                        You can find these search terms here:
-                                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                                        you don't find a specific author or title. Every search is limited by search terms.
-                                        */
-                }
-                <input type="text" placeholder="Search by title or author"/>
+      <Route path="/search" exact={true} render={() => <div className="search-books">
+          <div className="search-books-bar">
+            <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close
+            </a>
+            <div className="search-books-input-wrapper">
+              {/* NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-              </div>
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                  */
+              }
+              <input type="text" placeholder="Search by title or author"/>
+
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>}/>
+
+      <Route path="/" exact={true} render={() => <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              {this.state.bookshelfs.map(e => <BookShelf books={this.loadBooksFromBookShelf(e)} id={e} key={e} updateBook={this.updateBook}></BookShelf>)}
             </div>
-          </div>)
-          : (<div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {this.state.bookshelfs.map(e => <BookShelf books={this.loadBooksFromBookShelf(e)} id={e} key={e} updateBook={this.updateBook}></BookShelf>)}
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">
-                Add a book
-              </Link>
-            </div>
-          </div>)
-      }
+          </div>
+          <div className="open-search">
+            <Link to="/search">
+              Add a book
+            </Link>
+          </div>
+        </div>}/>
+
     </div>)
   }
 }

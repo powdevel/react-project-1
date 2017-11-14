11b580c474d28f74793cdd7e7c30884ee29a55c3
powdevel <luis.aveiga@gmail.com>
2017-11-13 19:23:47 -0500

    refactor: refactor some methods
    Refactoring some methods.

--- src/App.js
+++ src/App.js
@@ -6,50 +6,35 @@ import {Link, Route} from 'react-router-dom'
 import Search from './Search.js'
 import {AnimatedSwitch} from 'react-router-transition'
 
-function mapStyles(styles) {
-  return {opacity: styles.o}
-}
-
-// child matches will...
-const fadeTransition = {
-  atEnter: {
-    o: 1
-  },
-  atLeave: {
-    o: 0
-  },
-  atActive: {
-    o: 1
-  }
-};
-
 class BooksApp extends React.Component {
   state = {
     books: [],
-    bookshelfs: [],
-    loaded: false
+    bookshelfs: [
+      'currentlyReading', 'wantToRead', 'read'
+    ],
+    loaded: false,
+    message: 'Preparing contents...'
   }
 
   constructor(props) {
     super(props);
     this.loadBooks();
   }
-  
-  getBookShelfs(books) {
-    return ['currentlyReading', 'wantToRead', 'read'];
-  }
 
   loadBooks() {
-    BooksAPI.getAll().then(books => {
-      this.setState({books: books, bookshelfs: this.getBookShelfs(books), loaded: true});
-    });
+    BooksAPI
+      .getAll()
+      .then(books => {
+        this.setState({books: books, loaded: true});
+      })
+      .catch(() => {
+        this.setState({message: 'Unable to fetch contents... Do you have internet?'})
+      });
   }
 
   getBooks = () => this.state.books;
 
-  booksFromBookShelf(bookshelf, books) {
-    return books.filter(e => e.shelf === bookshelf);
-  }
+  booksFromBookShelf = (bookshelf, books) => books.filter(e => e.shelf === bookshelf);
 
   changeShelfFromBook = (e, title, shelf) => {
     e.title === title && (e.shelf = shelf)
@@ -58,7 +43,10 @@ class BooksApp extends React.Component {
 
   updateBook = (book, shelf) => {
     BooksAPI.update(book, shelf)
-    let books = this.state.books.map((e) => this.changeShelfFromBook(e, book.title, shelf))
+    let books = this
+      .state
+      .books
+      .map((e) => this.changeShelfFromBook(e, book.title, shelf))
     this.setState({books: books});
   }
 
@@ -70,18 +58,24 @@ class BooksApp extends React.Component {
       </div>
 
       <div className="relative-wrapper">
-        <AnimatedSwitch atEnter={fadeTransition.atEnter} atLeave={fadeTransition.atLeave} atActive={fadeTransition.atActive} mapStyles={mapStyles} className="route-wrapper">
+        <AnimatedSwitch atEnter={{o:1}} atLeave={{o:0}} atActive={{o:1}} mapStyles={styles => ({opacity:styles.o})} className="route-wrapper">
           <Route path="/search" exact={true} render={() => <Search getBooks={this.getBooks} updateBook={this.updateBook}/>}/>
-
           <Route path="/" exact={true} render={() => <div className="list-books">
               <div className="list-books-content">
-                {this.state.bookshelfs.map(e => <BookShelf books={this.booksFromBookShelf(e, this.state.books)} id={e} key={e} updateBook={this.updateBook}></BookShelf>)}
-                {!this.state.loaded && (<div className="book-loader">Preparing contents...</div>)}
-              </div>
-              <div className="open-search">
-                <Link to="/search">
-                  Add a book
-                </Link>
+                {
+                  this.state.loaded && this
+                    .state
+                    .bookshelfs
+                    .map(e => <BookShelf books={this.booksFromBookShelf(e, this.state.books)} id={e} key={e} updateBook={this.updateBook}></BookShelf>)
+                }
+                {
+                  this.state.loaded && (<div className="open-search">
+                    <Link to="/search">
+                      Add a book
+                    </Link>
+                  </div>)
+                }
+                {!this.state.loaded && (<div className="book-loader">{this.state.message}</div>)}
               </div>
             </div>}/>
         </AnimatedSwitch>

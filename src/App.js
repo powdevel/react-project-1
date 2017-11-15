8a03bf728ae0b6866c68579f5bbcaa0671a1a010
powdevel <luis.aveiga@gmail.com>
2017-11-15 16:19:48 -0500

    feat: format with prettier and add loading indicator
    The main thing is the added loading indicator and styles.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,130 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import BookShelf from './BookShelf'
-import {Link, Route} from 'react-router-dom'
-import Search from './Search.js'
-import {AnimatedSwitch} from 'react-router-transition'
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import BookShelf from "./BookShelf";
+import { Link, Route } from "react-router-dom";
+import Search from "./Search.js";
+import { AnimatedSwitch } from "react-router-transition";
 
 class BooksApp extends React.Component {
   state = {
     books: [],
-    bookshelfs: [
-      'currentlyReading', 'wantToRead', 'read'
-    ],
+    bookshelfs: ["currentlyReading", "wantToRead", "read"],
     loaded: false,
-    message: 'Preparing contents...'
-  }
+    loading: 0,
+    message: "Preparing contents..."
+  };
 
   constructor(props) {
     super(props);
     this.loadBooks();
   }
 
+  loadingClass = () => (this.state.loading ? "loading" : "");
+
+  loadingMORE = () =>
+    this.setState({
+      loading: this.state.loading + 1
+    });
+
+  loadingLESS = () =>
+    this.setState({
+      loading: this.state.loading > 1 ? this.state.loading - 1 : 0
+    });
+
+  loadingOFF = () => this.setState({ loading: 0 });
+
   loadBooks() {
-    BooksAPI
-      .getAll()
+    BooksAPI.getAll()
       .then(books => {
-        this.setState({books: books, loaded: true});
+        this.setState({ books: books, loaded: true });
       })
       .catch(() => {
-        this.setState({message: 'Unable to fetch contents... Are you connected to the Internet?'})
+        this.setState({
+          message: "Unable to fetch contents... Are you connected to the Internet?"
+        });
       });
   }
 
   getBooks = () => this.state.books;
 
-  fromBookShelf = function ( book ) { return book.shelf === this }
+  fromBookShelf = function(book) {
+    return book.shelf === this;
+  };
+
+  findBookByID = function(book) {
+    return book.id === this.id;
+  };
+
+  updateBookShelf = function(book) {
+    book.shelf = book.id === this.id ? this.shelf : book.shelf;
+    return book;
+  };
 
   updateBook = (book, shelf) => {
-    BooksAPI
-      .update(book, shelf)
-      .then(() => BooksAPI.getAll())
-      .then(books => this.setState({'books': books}));
-  }
+    book.shelf = shelf;
+    this.loadingMORE();
+    BooksAPI.update(book, shelf)
+      .then(() => {
+        let iHaveThatBook = this.state.books.filter(this.findBookByID, book);
+        let updatedBooks = iHaveThatBook.length ? this.state.books.map(this.updateBookShelf, book) : this.state.books.concat(book);
+        this.setState({ books: updatedBooks });
+        this.loadingLESS();
+      })
+      .catch(() => {
+        this.loadingLESS();
+      });
+  };
 
   render() {
+    return (
+      <div className="app">
+          <div className="list-books-title">
+              <h1>
+                  MyReads
+                  <span className={this.loadingClass()} />
+              </h1>
+          </div>
 
-    return (<div className="app">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
+          <div className="relative-wrapper">
+              <AnimatedSwitch
+                  atEnter={{ o: 1 }}
+                  atLeave={{ o: 0 }}
+                  atActive={{ o: 1 }}
+                  mapStyles={styles => ({ opacity: styles.o })}
+              className="route-wrapper">
+                  <Route
+                      path="/search"
+                      exact={true}
+                      render={() => (
+                          <Search getBooks={this.getBooks} updateBook={this.updateBook} loadingMORE={this.loadingMORE} loadingOFF={this.loadingOFF} />
+                      )}
+                  />
+                  <Route
+                      path="/"
+                      exact={true}
+                      render={() => (
+                          <div className="list-books">
+                              <div className="list-books-content">
+                                  {this.state.loaded &&
+                                      this.state.bookshelfs.map(e => (
+                                          <BookShelf books={this.state.books.filter(this.fromBookShelf, e)} id={e} key={e} updateBook={this.updateBook} />
+                                      ))}
+                                  {this.state.loaded && (
+                                      <div className="open-search">
+                                          <Link to="/search">Add a book</Link>
+                                      </div>
+                                  )}
+                                  {!this.state.loaded && <div className="book-loader">{this.state.message}</div>}
+                              </div>
+                          </div>
+                      )}
+                  />
+              </AnimatedSwitch>
+          </div>
       </div>
-
-      <div className="relative-wrapper">
-        <AnimatedSwitch atEnter={{
-            o: 1
-          }} atLeave={{
-            o: 0
-          }} atActive={{
-            o: 1
-          }} mapStyles={styles => ({opacity: styles.o})} className="route-wrapper">
-          <Route path="/search" exact={true} render={() => <Search getBooks={this.getBooks} updateBook={this.updateBook}/>}/>
-          <Route path="/" exact={true} render={() => <div className="list-books">
-              <div className="list-books-content">
-                {
-                  this.state.loaded && this
-                    .state
-                    .bookshelfs
-                    .map(e => <BookShelf books={ this.state.books.filter( this.fromBookShelf, e ) } id={e} key={e} updateBook={this.updateBook}></BookShelf>)
-                }
-                {
-                  this.state.loaded && (<div className="open-search">
-                    <Link to="/search">
-                      Add a book
-                    </Link>
-                  </div>)
-                }
-                {!this.state.loaded && (<div className="book-loader">{this.state.message}</div>)}
-              </div>
-            </div>}/>
-        </AnimatedSwitch>
-      </div>
-    </div>)
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/Search.js
+++ src/Search.js
@@ -1,90 +1,114 @@
-import React from 'react'
-import Book from './Book'
-import {Link} from 'react-router-dom'
-import DebounceInput from 'react-debounce-input'
-import {CSSTransition, TransitionGroup} from 'react-transition-group'
-import * as BooksAPI from './BooksAPI'
+import React from "react";
+import Book from "./Book";
+import { Link } from "react-router-dom";
+import DebounceInput from "react-debounce-input";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import * as BooksAPI from "./BooksAPI";
 
-const Fade = ({
-  children,
-  ...props
-}) => (<CSSTransition {...props} timeout={250} classNames="fade">
-  {children}
-</CSSTransition>);
+const Fade = ({ children, ...props }) => (
+    <CSSTransition {...props} timeout={250} classNames="fade">
+        {children}
+    </CSSTransition>
+);
 
 class Search extends React.Component {
+    state = {
+        query: "",
+        filteredBooks: []
+    };
 
-  state = {
-    query: '',
-    filteredBooks: []
-  }
+    // To "break" the promise of fetching contents
+    // is the search token is different
+    searchToken = 0;
 
-  // To "break" the promise of fetching contents
-  // is the search token is different
-  searchToken = 0;
+    processData = (query, fetchedBooks) => {
+        this.props.loadingOFF(); // Hides loading indicator
+        let myBooks = this.props.getBooks(); // Getting currently books on shelf
 
-  updateSearchedBooks = (query) => {
-    let token = ++this.searchToken;
-
-    if (query === "") {
-      this.setState({query: query, filteredBooks: []});
-      return;
-    }
-
-    BooksAPI
-      .search(query, 20)
-      .then(searchedBooks => {
-
-        if (token === this.searchToken) {
-          let myBooks = this
-            .props
-            .getBooks();
-
-          searchedBooks = searchedBooks.map(book => {
+        // Updates the shelf of a book in the search result
+        fetchedBooks = fetchedBooks.map(book => {
             let iHaveThatBook = myBooks.filter(myBook => myBook.id === book.id);
-            iHaveThatBook.length
-              ? book = iHaveThatBook[0]
-              : book.shelf = 'none';
+            book.shelf = iHaveThatBook.length ? iHaveThatBook[0].shelf : "none";
             return book;
-          });
-          this.setState({query: query, filteredBooks: searchedBooks});
+        });
+        this.changeSearchResults(query, fetchedBooks);
+    };
+
+    changeSearchResults = (query, books) => {
+        this.setState({ query: query, filteredBooks: [] });
+    };
+
+    onQueryChange = query => {
+        let token = ++this.searchToken;
+
+        if (query === "") {
+            // this.setState({query: query, filteredBooks: []});
+            this.changeSearchResults("", []);
+            this.props.loadingOFF(); // Hides loading indicator
+            return;
         }
-      })
-      .catch(() => {
-        if (token == this.searchToken) {
-          this.setState({query: query, filteredBooks: []});
-        }
-      });
 
-  }
+        this.props.loadingMORE();
+        BooksAPI.search(query, 20)
+            .then(fetchedBooks => {
+                // Only pass if it's the last searched term
+                if (token === this.searchToken) {
+                    this.processData(query, fetchedBooks);
+                }
+            })
+            .catch(() => {
+                if (token === this.searchToken) {
+                    // if something's wrong then show empty results
+                    this.changeSearchResults(query, []);
+                }
+            });
+    };
 
-  render() {
-    return (<div className="search-books">
-
-      <div className="search-books-bar">
-        <Link className="close-search" to="/">
-          Close
-        </Link>
-        <div className="search-books-input-wrapper">
-          <DebounceInput debounceTimeout={300} onChange={event => this.updateSearchedBooks(event.target.value.trim())}/>
-        </div>
-      </div>
-      <div className="search-books-results">
-        <TransitionGroup component="ol" className="books-grid">
-          {
-            this.state.query !== "" && this
-              .state
-              .filteredBooks
-              .map(e => <Fade key={e.title}>
-                <li><Book data={e} updateBook={this.props.updateBook}/></li>
-              </Fade>)
-          }
-        </TransitionGroup>
-        {this.state.query !== "" && this.state.filteredBooks.length === 0 && (<p className="search-message">No results found for your search</p>)}
-        {this.state.query === "" && (<p className="search-message">Your search results will appear right here :)</p>)}
-      </div>
-    </div>)
-  }
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">
+                        Close
+                    </Link>
+                    <div className="search-books-input-wrapper">
+                        <DebounceInput
+                            debounceTimeout={300}
+                            onChange={event =>
+                                this.onQueryChange(event.target.value.trim())
+                            }
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <TransitionGroup component="ol" className="books-grid">
+                        {this.state.query !== "" &&
+                            this.state.filteredBooks.map(e => (
+                                <Fade key={e.title}>
+                                    <li>
+                                        <Book
+                                            data={e}
+                                            updateBook={this.props.updateBook}
+                                        />
+                                    </li>
+                                </Fade>
+                            ))}
+                    </TransitionGroup>
+                    {this.state.query !== "" &&
+                        this.state.filteredBooks.length === 0 && (
+                            <p className="search-message">
+                                No results found for your search
+                            </p>
+                        )}
+                    {this.state.query === "" && (
+                        <p className="search-message">
+                            Your search results will appear right here :)
+                        </p>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Search;

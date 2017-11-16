49eb0eb6e79d26ce04776c3c5476f54d6588755e
powdevel <luis.aveiga@gmail.com>
2017-11-16 01:00:11 -0500

    refactor: replace regex with more undestandable code

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,31 +4,34 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const Fade = ({ children, ...props }) => (
   <CSSTransition {...props} timeout={250} classNames="fade">
-      {children}
+    {children}
   </CSSTransition>
 );
 
 class BookShelf extends React.Component {
+  firstLetterUp = title => title.charAt(0).toUpperCase() + title.slice(1);
+  spaceBeforeCap = title => title.split("").reduce((title, letter) => title + (letter.toUpperCase() === letter ? " " : "") + letter, "");
+  
   render() {
     return (
       <div className="bookshelf">
-          <h2 className="bookshelf-title">{this.props.id.replace(/([a-z](?=[A-Z]))/g, "$1 ")}</h2>
-          <div className="bookshelf-books">
-              <TransitionGroup component="ol" className="books-grid">
-                  {this.props.books.map(e => (
-                      <Fade key={e.id}>
-                          <li>
-                              <Book data={e} updateBook={this.props.updateBook} />
-                          </li>
-                      </Fade>
-                  ))}
-              </TransitionGroup>
-              {this.props.books.length === 0 && (
-                  <center>
-                      <p>No books found in this category</p>
-                  </center>
-              )}
-          </div>
+        <h2 className="bookshelf-title">{this.spaceBeforeCap(this.firstLetterUp(this.props.id))}</h2>
+        <div className="bookshelf-books">
+          <TransitionGroup component="ol" className="books-grid">
+            {this.props.books.map(e => (
+              <Fade key={e.id}>
+                <li>
+                  <Book data={e} updateBook={this.props.updateBook} />
+                </li>
+              </Fade>
+            ))}
+          </TransitionGroup>
+          {this.props.books.length === 0 && (
+            <center>
+              <p>No books found in this category</p>
+            </center>
+          )}
+        </div>
       </div>
     );
   }
